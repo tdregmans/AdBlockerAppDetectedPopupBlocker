ff4de8f1bccdac83538f2d1d12c9ca830d4d5204
Thijs Dregmans <tdregmans+localGit@protonmail.com>
2025-01-28 20:16:57 +0100

    temporary backup

--- AdBlockerAppDetectedPopupBlockerApp.js
+++ AdBlockerAppDetectedPopupBlockerApp.js
@@ -5,5 +5,54 @@ document.body.style.border = "5px solid red";
 //     gopro_popup_elements[0].remove();
 
 
+// var elem = document.getElementById('overlap-manager-root');
+//     elem.parentNode.removeChild(elem);
+
+// const selectElement = document.querySelector("body");
+// // const result = document.querySelector(".result");
+
+// selectElement.addEventListener("change", (event) => {
+//     console.log("should work!");
+// });
+    
+
+    console.log("executed!");
+
+
+/// source: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+
+const target = document.body;
+const child = document.getElementById("overlap-manager-root");
+
+const observer = new MutationObserver((mutations) => {
+    
 var elem = document.getElementById('overlap-manager-root');
     elem.parentNode.removeChild(elem);
+
+//   mutations.forEach((mutation) => {
+//     console.log(mutation.type, mutation.target.id, mutation.attributeName);
+
+//     if (mutation.type === "childList" && mutation.target.id === "target") {
+//       // After receiving the notification that the child was removed,
+//       // further modifications to the detached subtree no longer trigger the observer.
+//     //   child.setAttribute("data-bar", "");
+//       console.log("done");
+//     }
+//   });
+});
+
+observer.observe(target, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
+
+// target.removeChild(child);
+// This change happens before the "childList target" notification is delivered,
+// so it will also trigger the observer.
+// child.setAttribute("data-foo", "");
+
+// Output:
+// childList target null
+// attributes child data-foo
+// There is no "attributes child data-bar" notification.
