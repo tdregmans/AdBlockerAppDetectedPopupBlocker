0fb59a22c9ddeb0bf859ab2a8e2f8bd0b0ad69ae
tdregmans <tdregmans+github@protonmail.com>
2025-02-09 19:31:42 +0100

    Cleanup code and manifest

--- AdBlockerAppDetectedPopupBlockerApp.js
+++ AdBlockerAppDetectedPopupBlockerApp.js
@@ -1,29 +1,44 @@
-/// source: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+/*
+ * AdBlockerAppDetectedPopupBlockerApp
+ * AdBlockerAppDetectedPopupBlockerApp.js
+ * 
+ * Author: tdregmans
+ * Version: 1.0
+ * Last edited: 2025-02-09
+ * 
+**/
 
-const target = document.body;
-const child = document.getElementById("overlap-manager-root");
+/*********************************************************************************
+ * Create MutationObserver on overlap-manager-root
+ * https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+*/
 
+const target = document.getElementById("overlap-manager-root");
+
+// Create observer
 const observer = new MutationObserver((mutations) => {
-    
   var elem = document.querySelector('div[data-dialog-name="gopro"]');
-
-  if( elem != null) {
-
-    console.log("removed!");
+  if (elem != null) {
     elem.remove();
+    console.log("Removed div!");
   } 
-
 });
 
+// Connect observer to target
 observer.observe(target, {
   attributes: true,
   childList: true,
   subtree: true,
 });
 
+/*********************************************************************************
+ * Remove is-not-pro classname 
+*/
 
-var elems = document.querySelectorAll("html");
+// get html element
+var html_element = document.querySelectorAll("html");
 
-[].forEach.call(elems, function(el) {
-    el.classList.remove("is-not-pro");
+// remove is-not-pro class from html element
+[].forEach.call(html_element, function(elem) {
+  elem.classList.remove("is-not-pro");
 });
